25bc08b90cfad22664ec3c48eec2c2e2f03b1291
tintedsky <hongbonew@gmail.com>
2017-04-12 23:06:57 -0400

    commit from local 2nd time

--- SaveKitten/ViewController.swift
+++ SaveKitten/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("hello woxld test from local and website same user")
+        print("hello woxld test from local second time")
     }
 
     override func didReceiveMemoryWarning() {
