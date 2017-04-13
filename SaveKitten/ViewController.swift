3d07dc970c076198716906b1b3fee742cd2ac3e3
tintedsky <hongbonew@gmail.com>
2017-04-12 22:42:50 -0400

    Update ViewController.swift

--- SaveKitten/ViewController.swift
+++ SaveKitten/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("hello woxld")
+        print("hello wox?ld")
     }
 
     override func didReceiveMemoryWarning() {
